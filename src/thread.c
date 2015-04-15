2daf1858e190ffae99ebefbe0c02a31729dd429a
JANVIER Yoann <yjanvier@biguine.pedago.ipb.fr>
2015-04-15 15:10:05 +0200

    Création queue pour les threads à freere

--- src/thread.c
+++ src/thread.c
@@ -8,8 +8,9 @@
 STAILQ_HEAD(Thread_Queue, thread_);
 
 struct Thread_Queue* thread_queue;
-thread_t thread_current;
-thread_t thread_to_free;
+struct Thread_Queue* queue_to_free;
+thread_t current_thread;
+//thread_t thread_to_free;
 thread_t old_thread;
 
 // Initialise la thread_queue
@@ -18,11 +19,14 @@ extern void init_thread_queue()
   STAILQ_INIT(thread_queue);
 }
 
+extern void init_queue_to_free(){
+  STAILQ_INIT(queue_to_free);
+}
 
 
 
 extern thread_t thread_self(){
-  return thread_current;
+  return current_thread;
 }
 
 extern int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
@@ -38,9 +42,6 @@ extern int thread_create(thread_t *newthread, void *(*func)(void *), void *funca
 
   *newthread = t;
 
-  //ucontext_t current_context;
-  //swapcontext(&current_context, &(t->context));
-  //return thread_yield();
   STAILQ_INSERT_TAIL(thread_queue, t, next);
 
   return 0;
@@ -48,40 +49,35 @@ extern int thread_create(thread_t *newthread, void *(*func)(void *), void *funca
 
 
 extern void thread_yield(void){
-  thread_current->status = WAITING;
-  old_thread = thread_current;
-  //Tester si la queue est vide si non:
-  //prendre la tete de la queue et la mettre dans thread_current
-  //Supprimer la tete et placer a tete au suivant de la queue
+  current_thread->status = WAITING;
+  old_thread = current_thread;
+
   if(thread_queue->stqh_first)
   {
-      thread_current = thread_queue->stqh_first;
+      current_thread = thread_queue->stqh_first;
       thread_queue->stqh_first = thread_queue->stqh_first->next.stqe_next;
   }
 
-  thread_current->status = RUNNING;
-  swapcontext(&(old_thread->context), &(thread_current->context));
+  current_thread->status = RUNNING;
+  swapcontext(&(old_thread->context), &(current_thread->context));
 
 }
 
 extern int thread_join(thread_t thread, void **retval){
-  //  wait();
-
-  //on déclare qu ele thread dont on attend la fin de l'éxécution est thread
-  //thread_current->previous = thread;
+  
   //On le met en attente
-  thread_current->status= WAITING;
+  current_thread->status= WAITING;
   //On met le thread en running si ce n'est déja fait
   thread->status=RUNNING;
-  //On déclare que le prochain à exécuter après thread est thread_current
-  thread->next.stqe_next = thread_current;
+  //On déclare que le prochain à exécuter après thread est current_thread
+  thread->next.stqe_next = current_thread;
 
   if(retval!=NULL){
     *retval=thread->retval;
   }
 
-  free((thread_to_free->context).uc_stack.ss_sp);
-  free(thread_to_free);
+  free((thread->context).uc_stack.ss_sp);
+  free(thread);
   if(!retval)
       return 0;
   return -1;
@@ -89,9 +85,11 @@ extern int thread_join(thread_t thread, void **retval){
 
 
 extern void thread_exit(void *retval) __attribute__ ((__noreturn__)){
-  thread_to_free=thread_current;
-  thread_to_free->next=NULL;
-  thread_to_free->status= FINISHED;
-  thread_current=thread_current->next;
+  current_thread->status= FINISHED;
+  thread_t tmp_t= current_thread;
+  current_thread=current_thread->next;
+  current_thread->status=READY;
+  STAILQ_INSERT_TAIL(queue_to_free,tmp_t,next);
+  //Supprime t'on la tete de la queue? ou garde t'on tous les threads termines?
   setcontext((*thread_queue)->context);
 };
