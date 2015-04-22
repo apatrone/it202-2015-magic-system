98321203168200af69a916467c413efe8792dcaf
apatrone <agnespatrone@hotmail.com>
2015-04-22 19:42:25 +0200

    mergedes deux thread.c

--- src/thread.c
+++ src/thread.c
@@ -1,3 +1,4 @@
+
 #include "thread.h"
 #include <sys/queue.h>
 #include <ucontext.h>
@@ -14,33 +15,35 @@ int thread_init = 0;
 struct threadqueue thread_queue;
 struct threadqueue queue_to_free;  //à changer sans *
 thread_t current_thread;
-//thread_t thread_to_free;
 thread_t old_thread;
 thread_t main_thread;
 
 // Initialise la thread_queue
 
+
 void init_thread(thread_t *t){
   thread_s *thd = malloc(sizeof(thread_s));
-  printf("thread_init called\n");
+
   getcontext(&(thd->context));
   (thd->context).uc_stack.ss_size=64*1024;
   (thd->context).uc_stack.ss_sp = malloc((thd->context).uc_stack.ss_size);
   (thd->context).uc_stack.ss_flags = 0;
-   
   (thd->context).uc_link=NULL;
+  thd->waiting_by=NULL;
   thd->retval=NULL;
+
   *t = thd;
 }
-
+ 
 void init_thread_main(){
   printf("thread_init_main called !!!!!\n ");
   main_thread = malloc(sizeof(thread_s));
   getcontext(&(main_thread->context));
   main_thread->retval=NULL;
+  main_thread->waiting_by=NULL;
 }
 
-void libthread_init() {	  
+void libthread_init() {	  //pas besoin d'allouer la pile pour le main thread car elle est déjà allouée
   STAILQ_INIT(&thread_queue);
   STAILQ_INIT(&queue_to_free);
   init_thread(&current_thread);
@@ -67,13 +70,11 @@ int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg){
   thread_t t;
   init_thread(&t);
  	
-
   makecontext(&(t->context), (void (*)(void))func, 1, funcarg); 
-  ///	t->retval=func(funcarg);
   (t->status) = READY;
 
   *newthread = t;
-  STAILQ_INSERT_TAIL(&thread_queue, t, next); //erreur de seg ici
+  STAILQ_INSERT_TAIL(&thread_queue, t, next);
 
   return 0;
 }
@@ -103,53 +104,87 @@ int thread_yield(void){
 
 }
 
-int thread_join(thread_t thread, void **retval){
+int thread_join(thread_t thread, void **retval)
+{ 
+  printf("join45\n");
+  printf("join\n");
+  printf("thread_init %i\n", thread_init);
   assert(thread_init);
-  //On le met en attente
-  current_thread->status= WAITING;	
-  //On met le thread en running si ce n'est déja fait
-  thread->status=RUNNING;
-  //On déclare que le prochain à exécuter après thread est current_thread
-  thread->next.stqe_next = current_thread;
-
-	
-  printf("retval: %x\n", *retval);
-  if(retval!=NULL){
-    *retval=thread->retval;
-  }	
-
-  if(thread != main_thread){
-    clean_finished_thread();
+	printf("join\n");
+  // Est-ce qu'il est attendu par quelqu'un d'autre ? (si oui on quitte)
+  if(thread->waiting_by!=NULL){
+    return -1;
+  } 
+  // Si non, c'est nous qui l'attendons et personne d'autre
+  else {
+ 	 current_thread->status = WAITING;
+ 	 thread->waiting_by = current_thread;
   }
+  
+  // Est-ce que le thread attendu est fini ?
+  if(thread->status != FINISHED) {
+    // Si le thread est ready, on lui donne la main pour finir au plus tot
+    if (thread->status == READY) {
+      STAILQ_REMOVE( &thread_queue, thread, thread_, next );
+      current_thread = thread;
+    }
+    // Sinon on donne la main a un autre
+    else {
+      current_thread = STAILQ_FIRST(&thread_queue);
+      STAILQ_REMOVE_HEAD(&thread_queue, next);
+    }
+    swapcontext(&(thread->context), &(current_thread->context));
+  }
+	
+  // Normalement on ne reviens ici que si le thread est fini
+  assert( thread->status == FINISHED );
+ 
+  // Recupere la valeur de retour
+  if(retval!=NULL){
+    *retval = thread->retval;
+  }	
+  
+  
+  // On libere les ressources allouées a thread
+  if(thread != main_thread){
+    free((thread->context).uc_stack.ss_sp);
+  }
+  free(thread);
 
   return 0;
 }
 
 
 
-void thread_exit(void *retval){
+void thread_exit(void *retval) {
   assert(thread_init);
   printf("exit\n");
-  current_thread->status= FINISHED;
-  current_thread->retval=retval;
-  printf("retval: %x\n", retval);
-  printf("->retval: %x\n", current_thread->retval);
 
-  thread_t tmp_t= current_thread;  printf("exit\n");
-  current_thread=current_thread->next.stqe_next;  
-  if(current_thread){
-    current_thread->status=READY;printf("boucle\n");
+  current_thread->retval = retval;
+  thread_t tmp_t = current_thread; 
+  
+  // Il y a un thread qui m'attend, je lui donne la main
+  if(tmp_t->waiting_by){
+  	fprintf(stderr, "Je donne la main\n");
+    current_thread = tmp_t->waiting_by;
+  }    
+  // Sinon je donne la main au premier qui est ready
+  else{
+  	fprintf(stderr, "Je donne la main\n");
+    current_thread = STAILQ_FIRST(&thread_queue);
+    STAILQ_REMOVE_HEAD(&thread_queue, next);
   }
 
   STAILQ_INSERT_TAIL(&queue_to_free,tmp_t,next);
-  printf("exit\n");
+  tmp_t->status = FINISHED;
+  assert(current_thread->status == READY);
+
   //Supprime t'on la tete de la queue? ou garde t'on tous les threads termines?
 
-  int rc=setcontext(&((thread_queue.stqh_first)->context)); 
+  int rc = setcontext(&(current_thread->context)); 
   if(rc==-1)
     perror("Error: setcontext");
 }
-
 
 void kill_main_thread(){
   printf("on free le main \n");
@@ -168,3 +203,4 @@ void clean_finished_thread(){
     free(current_thread);
   }
 }
+
