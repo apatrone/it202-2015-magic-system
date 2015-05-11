ee0fe4a79cda69aaeea07843967b7f1693362e56
JANVIER Yoann <yjanvier@chargers.pedago.ipb.fr>
2015-05-11 17:42:39 +0200

    .

--- src/preemption.c
+++ src/preemption.c
@@ -16,8 +16,11 @@ static void set_timer_preempt (long time_slice)
 {
   iti.it_value.tv_sec     = time_slice / 1000000000;
   iti.it_value.tv_nsec    = time_slice % 1000000000;
-  iti.it_interval.tv_sec  = iti.it_value.tv_sec;
-  iti.it_interval.tv_nsec = iti.it_value.tv_nsec;
+  //iti.it_interval.tv_sec  = iti.it_value.tv_sec;
+  //iti.it_interval.tv_nsec = iti.it_value.tv_nsec;
 
   timer_settime(timerid, 0, &iti, NULL);
 }
+
+
+
